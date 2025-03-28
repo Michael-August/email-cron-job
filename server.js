0511e83d1d661f859f04c0ea80525da411a4bdc2
Chibuike Onwugharam <michaelfrancis135@gmail.com>
2025-03-28 07:07:07 +0100

    Stop constant logging

--- server.js
+++ server.js
@@ -43,8 +43,14 @@ const SESClientConfig = new SESClient({
 
 async function processEmailQueue() {
 	const batch = await redis.lRange(QUEUE_KEY, 0, BATCH_SIZE - 1); // Get first 50
+	lastLogTime = 0;
 	if (batch.length === 0) {
-		console.log("✅ No emails to process.");
+		const now = Date.now();
+		if (now - lastLogTime > 60 * 60 * 1000) {
+			// Log every 10 minutes
+			console.log("✅ No emails to process.");
+			lastLogTime = now;
+		}
 		return;
 	}
 
